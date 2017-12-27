879abf368a4e809182240f2aec07cd6e3f4f7d06
Kip Tatum <kiptatum@gmail.com>
2017-12-27 14:07:30 -0800

    stuff

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kipta
+ * Date: 12/27/2017
+ * Time: 12:29 PM
+ */
+
+$servername = "compassbimysqlsvr.mysql.database.azure.com";
+$username = "kiplogin@compassbimysqlsvr";
+$password = "Sku11crush3r";
+//connect to database
+$conn = new PDO("mysql:host=$servername;dbname=compassbiprojecttracer", $username, $password);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "Connected successfully.  ";
