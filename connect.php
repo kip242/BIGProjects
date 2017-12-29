3209cef56d4e3dd52ffbfecdb1aabe0f4a778949
Kip Tatum <kiptatum@gmail.com>
2017-12-28 16:29:53 -0800

    update connect

--- connect.php
+++ connect.php
@@ -7,10 +7,10 @@
  */
 
 $servername = "compassbimysqlsvr.mysql.database.azure.com";
-$username = "kiplogin@compassbimysqlsvr";
-$password = "Sku11crush3r";
+$username = ;
+$password = ;
 //connect to database
 $conn = new PDO("mysql:host=$servername;dbname=compassbiprojecttracer", $username, $password);
 // set the PDO error mode to exception
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "Connected successfully.  ";
+echo "Connected successfully.";
