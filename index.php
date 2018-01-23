a53eea657144abda42168a2791d2872eef62ed91
Kip Tatum <kiptatum@gmail.com>
2018-01-23 10:05:21 -0800

    minor change

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ try {
 </head>
 <body>
 <br>
-<h1>B.I.G. Project Dashboard</</h1>
+<h1>B.I.G. Project Dashboard</h1>
 <div>
     <form action="adminLogin.php">
     <button class="adminButton">Admin Login</button>
