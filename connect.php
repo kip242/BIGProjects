6e00a21f92d4b3d88be5c09c0f307bcd64b8d09f
Kip Tatum <kiptatum@gmail.com>
2018-01-30 08:30:24 -0800

    working again

--- connect.php
+++ connect.php
@@ -6,17 +6,12 @@
  * Time: 12:46 PM
  */
 
+
 $servername = "compassbimysqlsvr.mysql.database.azure.com";
 $username = "kiplogin@compassbimysqlsvr";
 $password = "Sku11crush3r";
-
-//$servername = getenv('REMOTE_ADDR');
-//$username = getenv('UNAME');
-//$password = getenv('PASSWORD');
-
-
 //connect to database
-$conn = new PDO("mysmpassbiprojecttracer", $username, $password);
+$conn = new PDO("mysql:host=$servername;dbname=compassbiprojecttracer", $username, $password);
 // set the PDO error mode to exception
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
